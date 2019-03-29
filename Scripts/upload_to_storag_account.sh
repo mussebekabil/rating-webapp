0324586c055e66b2f0e8cede2c3180aed71eec1d
mussebekabil <musse.bekabil@nordcloud.com>
2019-03-29 13:05:43 +0200

    Added upload linktemplate script

---
+++ Scripts/upload_to_storag_account.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# A simple Azure Storage example script
+
+export AZURE_STORAGE_ACCOUNT=<storageaccountname>
+export AZURE_STORAGE_KEY=<storageaccountkey>
+export container_name=linkedtemplatecontainer
+
+echo "Creating the container..."
+az storage container create --name $container_name
+
+echo "Clone templates from git ..." 
+git clone https://github.com/mussebekabil/rating-webapp.git ~/clouddrive/templates
+
+echo "Clean up unnecessary files ..." 
+rm -rf ~/clouddrive/templates/.git && rm ~/clouddrive/templates/.gitignore
+
+echo "Uploading the file..."
+az storage blob upload-batch -s ~/clouddrive/templates -d $container_name 
+
+echo "Clean up working directory ..." 
+rm -rf ~/clouddrive/templates
+
+echo "Listing the blobs..."
+az storage blob list --container-name $container_name --output table
+
+echo "Done"
